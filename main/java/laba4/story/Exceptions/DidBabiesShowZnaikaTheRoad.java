6b0b87212a70eeceadf7545969f553f5f5a5464d
OlgaGala <75944353+OlgaGala@users.noreply.github.com>
2021-01-18 17:45:43 +0300

    Delete main directory

--- main/java/laba4/story/Exceptions/DidBabiesShowZnaikaTheRoad.java
+++
@@ -1,7 +0,0 @@
-package laba4.story.Exceptions;
-
-public class DidBabiesShowZnaikaTheRoad extends RuntimeException {
-    public DidBabiesShowZnaikaTheRoad(String message) {
-        super(message);
-    }
-}
